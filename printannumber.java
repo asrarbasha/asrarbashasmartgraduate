0be269a75db25709acc1d8af15031b6bffdfb7e5
asrarbasha <113083057+asrarbasha@users.noreply.github.com>
2022-10-04 10:23:12 -0700

    Add files via upload

---
+++ printannumber.java
@@ -0,0 +1,15 @@
+package multiplication;
+import java.util.Scanner;
+
+public class printannumber {
+	public static void main(String[]args) {
+		Scanner reader=new Scanner(System.in);
+		System.out.println("enter a number:");
+		int number=reader.nextInt();
+		System.out.println("you entered:"+number);
+	}
+}
+	
+	
+
+
