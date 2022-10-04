50d7a60ed7a238ff4efcbae594065848de84c63e
asrarbasha <113083057+asrarbasha@users.noreply.github.com>
2022-10-04 10:23:40 -0700

    Add files via upload

---
+++ printasciivalue.java
@@ -0,0 +1,18 @@
+package multiplication;
+
+public class printasciivalue {
+	public static void main(String[]args) {
+		char ch='H';
+		int ascii=ch;
+		int castascii=(int)ch;
+		System.out.println("the ascii value of"+ch+"is:"+ascii);
+		System.out.println("the ascii value of"+ch+"is:"+castascii);
+	}
+}
+		
+		
+		
+		
+	
+
+
