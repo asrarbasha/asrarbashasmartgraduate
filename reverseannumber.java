c7755b34fca1417536f92a0120a5cc45ea216b19
asrarbasha <113083057+asrarbasha@users.noreply.github.com>
2022-10-04 10:31:38 -0700

    Add files via upload

---
+++ reverseannumber.java
@@ -0,0 +1,23 @@
+package smartgraduates;
+import java.util.Scanner;
+public class reverseannumber {
+	public static void main(String[]args) {
+		int num=0;
+		int reversenum=0;
+		System.out.println("input your number and press enter:");
+		Scanner in =new Scanner(System.in);
+		num=in.nextInt();
+		while (num !=0)
+		{
+			reversenum=reversenum*10;
+		reversenum=reversenum +num%10;
+		num=num/10;
+	}
+	System.out.println("the reverse of the input number is:"+reversenum);
+}
+}
+
+
+	
+
+
