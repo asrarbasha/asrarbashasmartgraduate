304ca880adbe45fbec0742d2f81d37b5b184233c
asrarbasha <113083057+asrarbasha@users.noreply.github.com>
2022-10-04 10:32:12 -0700

    Add files via upload

---
+++ sumofarrayvalues.java
@@ -0,0 +1,19 @@
+package smartgraduates;
+
+public class sumofarrayvalues {
+	public static void main(String[]args) {
+		int[]array= {10,20,30,40,50,10};
+		int sum=0;
+		for(int num:array) {
+			sum=sum+num;
+		}
+		System.out.println("the sum of array elements:"+sum);
+	}
+	
+		
+		
+		
+	
+	
+
+}
