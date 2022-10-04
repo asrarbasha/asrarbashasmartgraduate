0d30c6e67e908051b2172f9f74deebef66cdc570
asrarbasha <113083057+asrarbasha@users.noreply.github.com>
2022-10-04 10:24:50 -0700

    Add files via upload

---
+++ swaptwonumbersexercise.java
@@ -0,0 +1,29 @@
+package multiplication;
+
+public class swaptwonumbersexercise {
+	public static void main(String[]args) {
+		float first=4.50f, second=5.50f ;
+		System.out.println("--before swap--");
+		System.out.println("first number="+first);
+		System.out.println("second number="+second);
+		float temporary=first;
+		first=second;
+		second=temporary;
+		System.out.println("--after swap--");
+		System.out.println("first number="+first);
+		System.out.println("second number="+second);
+	}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+
+
