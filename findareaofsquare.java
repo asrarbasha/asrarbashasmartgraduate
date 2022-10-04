c6c9adf0a983707df5133bdd8bdd082c36c987dc
asrarbasha <113083057+asrarbasha@users.noreply.github.com>
2022-10-04 10:30:12 -0700

    Add files via upload

---
+++ findareaofsquare.java
@@ -0,0 +1,17 @@
+package smartgraduates;
+import java.util.Scanner;
+
+public class findareaofsquare {
+	public static void masin(String[]args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter the side of the square:");
+		double side=scanner.nextDouble();
+		double area=side*side;
+		System.out.println("area of the square is:"+area);
+		
+		
+		
+		
+	}
+
+}
