43d719c57102581c23eaac5818bb2c10713233e6
asrarbasha <113083057+asrarbasha@users.noreply.github.com>
2022-10-04 10:09:36 -0700

    Add files via upload

---
+++ basic2.java
@@ -0,0 +1,22 @@
+package multiplication;
+import java.util.Scanner;
+
+
+public class basic2 {
+	public static void main(String[]args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter value for i...:");
+		int i=scan.nextInt();
+		System.out.println("enter value for j...:");
+		int j=scan.nextInt();
+		int mul=i*j;
+		System.out.println("the multiplicationof i and j is:"+mul);
+	}
+}
+		
+	
+	
+	
+	
+
+
